de4081b8b7391f1c3125aab49855550fab722724
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-21 20:44:29 -0300

    chore: update eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-plugin-prettier'
+
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +24,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +36,14 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',
+      'no-underscore-dangle': 'off',
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'react/prop-types': 'off',
+      'no-process-exit': 'off',
+      'object-shorthand': 'off',
+      'class-methods-use-this': 'off',
     },
   },
-]
+];
