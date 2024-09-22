fe2f6ae4b9b1944641f00070afc387ca0b7ca853
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-21 21:14:34 -0300

    feat: add boilerplate React components for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { Text } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+
+// Components
+import HomePage from './pages/HomePage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
 function App() {
   return (
-    <Text
-      as="h1"
-      bgGradient="linear(to-l, #7928CA, #FF0080)"
-      bgClip="text"
-      fontSize="4xl"
-      fontWeight="bold"
-      p={2}
-    >
-      Hello World!
-    </Text>
+    <Box minH="100vh">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Catch-all route for 404 Not Found page */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Box>
   );
 }
 
