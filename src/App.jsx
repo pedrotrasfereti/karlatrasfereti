282b739a1c50b7720fcc721aabfb8c6cf79d1f98
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-21 21:01:12 -0300

    feat: test hello world with Chakra UI Text component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Text } from '@chakra-ui/react';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Text
+      as="h1"
+      bgGradient="linear(to-l, #7928CA, #FF0080)"
+      bgClip="text"
+      fontSize="4xl"
+      fontWeight="bold"
+      p={2}
+    >
+      Hello World!
+    </Text>
+  );
 }
 
 export default App;
