c8f20ce21f318f9454a5213beb12cbb038d91264
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-28 19:58:27 -0300

    feat: add Navbar to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { Box } from '@chakra-ui/react';
+import { Box, useColorModeValue } from '@chakra-ui/react';
 import { Route, Routes } from 'react-router-dom';
 
 // Components
+import Navbar from './components/Navbar.jsx';
 import HomePage from './pages/HomePage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 
 function App() {
+  const bgColor = useColorModeValue('surface.light', 'surface.dark');
+
   return (
-    <Box minH="100vh">
+    <Box bg={bgColor} minH="100vh">
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         {/* Catch-all route for 404 Not Found page */}
