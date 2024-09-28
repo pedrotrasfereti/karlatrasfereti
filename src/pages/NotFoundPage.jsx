a3e77bd24d1dff9fc236c7827cc52efa9f0cc613
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-28 20:29:42 -0300

    feat(NotFoundPage): add text and return to home button

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,30 @@
+import { Button, Container, Flex, Heading, VStack } from '@chakra-ui/react';
+import { FaArrowLeft } from 'react-icons/fa';
+
+import { Link } from 'react-router-dom';
+
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <Container as="main" minW="100%" px={0}>
+      <Flex alignItems="center" justifyContent="center" minH="100vh" p={6}>
+        <VStack spacing={14}>
+          <Heading textAlign="center">Página não encontrada ☹️</Heading>
+
+          <Button
+            leftIcon={<FaArrowLeft className="icon" />}
+            _hover={{
+              '.icon': {
+                transform: 'translateX(-30%)',
+                transition: 'all 0.3s ease',
+              },
+            }}
+          >
+            <Link to="/">Voltar para Home</Link>
+          </Button>
+        </VStack>
+      </Flex>
+    </Container>
+  );
 }
 
 export default NotFoundPage;
