c6aca15cbc52cd505a853b8fa8401ff57246e75a
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-28 20:02:50 -0300

    feat(HomePage): add all sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,22 @@
+import { VStack } from '@chakra-ui/react';
+import About from '../components/About.jsx';
+import Hero from '../components/Hero.jsx';
+import Skills from '../components/Skills.jsx';
+import Services from '../components/Services.jsx';
+import Contact from '../components/Contact.jsx';
+import Footer from '../components/Footer.jsx';
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <VStack as="main" spacing={0}>
+      <Hero />
+      <About />
+      <Skills />
+      <Services />
+      <Contact />
+      <Footer />
+    </VStack>
+  );
 }
 
 export default HomePage;
