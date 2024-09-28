96a0f8cc25afeda983014c3aef5d54534e998df8
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-09-28 19:55:18 -0300

    feat(main): import theme and font variables

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,24 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+
+// Theme
+import '@fontsource/open-sauce-sans/300.css';
+import '@fontsource/open-sauce-sans/400.css';
+import '@fontsource/open-sauce-sans/500.css';
+import '@fontsource/open-sauce-sans/600.css';
+import '@fontsource/open-sauce-sans/700.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import theme from './theme.js';
+
+// Components
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
