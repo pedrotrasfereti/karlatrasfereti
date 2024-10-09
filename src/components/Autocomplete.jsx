569ed68748ed5653a5dcbfce78d1026d0aa10fd3
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-10-09 15:25:04 -0300

    feat: create Autocomplete component

---
+++ src/components/Autocomplete.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+
+import {
+  Box,
+  Button,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
+
+import { SearchIcon } from '@chakra-ui/icons';
+
+import { CUIAutoComplete } from 'chakra-ui-autocomplete';
+
+function Autocomplete({
+  items,
+  label = '',
+  placeholder = '',
+  inputStyleProps = {},
+  onSetValue,
+}) {
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleSelectedItemsChange = (selectedItems) => {
+    if (selectedItems) {
+      if (selectedItems.length > 1) {
+        // Keep array length at 1 value
+        const lastSelected = selectedItems[selectedItems.length - 1];
+        setSelectedItems([lastSelected]);
+      } else {
+        setSelectedItems(selectedItems);
+      }
+    }
+  };
+
+  const renderCustomInput = (inputProps) => (
+    <HStack gap={2} w="full">
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <SearchIcon color="gray.300" />
+        </InputLeftElement>
+
+        <Input
+          autocomplete="off"
+          bg="white"
+          focusBorderColor="brand.700"
+          {...inputProps}
+        />
+      </InputGroup>
+
+      <Button
+        isDisabled={selectedItems.length === 0}
+        onClick={() => onSetValue(selectedItems[0].value)}
+      >
+        Confirmar
+      </Button>
+    </HStack>
+  );
+
+  return (
+    <Box px={8} py={4} w="full">
+      <CUIAutoComplete
+        label={label}
+        placeholder={placeholder}
+        disableCreateItem={true}
+        items={items}
+        highlightItemBg="green.50"
+        inputStyleProps={inputStyleProps}
+        tagStyleProps={{ display: 'none' }}
+        selectedItems={selectedItems}
+        renderCustomInput={renderCustomInput}
+        onSelectedItemsChange={(changes) =>
+          handleSelectedItemsChange(changes.selectedItems)
+        }
+      />
+    </Box>
+  );
+}
+
+export default Autocomplete;
