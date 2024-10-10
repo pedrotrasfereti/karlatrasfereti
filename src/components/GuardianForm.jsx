587d6c049f235867cbd1c61be4505f2ebe52e0a4
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-10-10 01:20:57 -0300

    feat(GuardianForm): validate phone number using RegEx

--- src/components/GuardianForm.jsx
+++ src/components/GuardianForm.jsx
@@ -9,8 +9,16 @@ function GuardianForm({ handleSubmit }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
 
+  const validatePhoneNumber = (phone) => {
+    const validRegex =
+      /^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/;
+    const sanitizedPhone = phone.replace(/[\s\-()]/g, '');
+
+    return validRegex.test(sanitizedPhone);
+  };
+
   const validForm = () => {
-    return name.length > 0 && email.length > 0 && phone.length > 0;
+    return name.length > 0 && email.length > 0 && validatePhoneNumber(phone);
   };
 
   return (
@@ -18,7 +26,7 @@ function GuardianForm({ handleSubmit }) {
       as="form"
       direction="column"
       justifyContent="space-between"
-      gap={10}
+      gap={8}
       w="full"
       onSubmit={handleSubmit}
     >
@@ -32,6 +40,7 @@ function GuardianForm({ handleSubmit }) {
             name="name"
             bg="white"
             focusBorderColor="brand.700"
+            placeholder="Escreva o seu nome"
           />
         </FormControl>
 
@@ -46,6 +55,7 @@ function GuardianForm({ handleSubmit }) {
             bg="white"
             mode="dark"
             focusBorderColor="brand.700"
+            placeholder="E-mail para contato"
           />
         </FormControl>
       </Flex>
@@ -61,16 +71,18 @@ function GuardianForm({ handleSubmit }) {
           {() => (
             <Input
               name="phone"
+              type="tel"
               bg="white"
               mode="dark"
               focusBorderColor="brand.700"
-              w={64}
+              maxW="16.25rem"
+              placeholder="Telefone para contato"
             />
           )}
         </InputMask>
       </FormControl>
 
-      <Button type="submit" isDisabled={!validForm()}>
+      <Button type="submit" isDisabled={!validForm()} mt={3}>
         Confirmar
       </Button>
     </Flex>
