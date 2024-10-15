b7ec182d302e027a600edf80a0d16dcae81c49e0
pedrotrasfereti <pedrotrasfereti@gmail.com>
2024-10-15 09:32:28 -0300

    feat(Hero): fade in hero image on mounted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,28 @@
+import { useEffect } from 'react';
+
 import {
   Box,
   Button,
   Container,
+  Fade,
   Flex,
   Heading,
   HStack,
   Image,
   Text,
+  useDisclosure,
 } from '@chakra-ui/react';
 
 import HeroImage from '../assets/hero.jpeg';
 
 function Hero() {
+  // Fade in element on mounted
+  const { isOpen, onToggle } = useDisclosure();
+
+  useEffect(() => {
+    onToggle();
+  }, []);
+
   return (
     <Container as="section" minW="100%" px={0} zIndex={2}>
       <HStack h="100vh">
@@ -45,14 +56,19 @@ function Hero() {
         </Flex>
 
         <Box flex="1" display={{ base: 'none', md: 'block' }}>
-          <Image
-            src={HeroImage}
-            alt="Cachorro deitado na grama feliz e sorridente"
-            h="100vh"
-            w="full"
-            objectFit="cover"
-            userSelect="none"
-          />
+          <Fade
+            in={isOpen}
+            transition={{ enter: { duration: 0.5 }, exit: { duration: 0 } }}
+          >
+            <Image
+              src={HeroImage}
+              alt="Cachorro deitado na grama feliz e sorridente"
+              h="100vh"
+              w="full"
+              objectFit="cover"
+              userSelect="none"
+            />
+          </Fade>
         </Box>
       </HStack>
     </Container>
